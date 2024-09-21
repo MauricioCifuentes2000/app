46f69f9c21fc953d5606afdfef8aab47eb82ab73
MauricioCifuentes2000 <maucifuentes00@live.com.mx>
2024-09-21 15:29:00 -0600

    modified:   app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+### Backend usando Pyhton Flask y MongoDB con JWT y Bcrypt ###
+### Universidad Anahuac Mayab
+### 31-08-2024, Fabricio Suárez
+### Prog de Dispositivos Móviles
+
+
 #importamos todo lo necesario para que funcione el backend
 from flask import Flask, request, jsonify
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from models import mongo, init_db
 from config import Config
 from bson.json_util import ObjectId
-from bson import ObjectId
 from flask_bcrypt import Bcrypt
 
 #Inicializamos la aplicación y usamos el config file
@@ -56,22 +61,83 @@ def login():
         return jsonify(access_token=access_token), 200
     else:
         return jsonify({"msg": "Credenciales incorrectas"}), 401
-    
-#creamos el endpoint protegido
+
+#Creamos el endpoint protegigo
 @app.route('/datos', methods=['POST'])
 @jwt_required()
 def datos():
-    data = request.get_json()
+    data= request.get_json()
     username = data.get('username')
 
-    usuario = mongo.db.users.find_one({"username":username},{"password":0})
+    usuario = mongo.db.users.find_one({"username":username}, {"password": 0})
 
-    if usuario:
-        usuario["_id"]=str(usuario["_id"])
-        return jsonify({"msg":"ususario encontrado", "usuario":usuario }),200
+    if usuario: 
+        usuario["_id"]= str(usuario["_id"])
+        return jsonify({"msg":"Usuario Encontrado", "Usuario":usuario}), 200
     else:
-        return jsonify({"msg":"usuario no encontrado"}),404
+        return jsonify({"msg": "Usuario NO encontrado"}), 404
+    
+# Endpoint para buscar usuario por el id del token
+@app.route('/userData', methods=['GET'])
+@jwt_required()
+def ruta_protegida():
+    # Obtener el ID del usuario desde el JWT
+    user_id = get_jwt_identity()
 
+    #El user_id que esta como str hay que convertirlo a ObjectId para poder hacer la busqueda
+    user_id = ObjectId(user_id)
+    
+    # Buscar en la base de datos usando el ID del usuario
+    user = mongo.db.users.find_one({'_id': user_id}, {"password": 0})
+
+    if user:
+        #Como _id es un objectid hay que volverlo str para poderlo mandar a un json
+        user['_id'] = str(user['_id'])
+        return jsonify({'message': 'Usuario encontrado', 'user': user}), 200
+    else:
+        return jsonify({'message': 'Usuario no encontrado'}), 404
+    
+#Endpoint para crear cars
+@app.route('/addCars', methods=['POST'])
+@jwt_required()
+def addCars():
+    # Obtener el ID del usuario desde el JWT
+    user_id = get_jwt_identity()
+
+    #El user_id que esta como str hay que convertirlo a ObjectId 
+    user_id = ObjectId(user_id)
+
+    car_data = request.get_json()
+    car_data['user_id'] = user_id
+
+    result = mongo.db.cars.insert_one(car_data)
+    if result.acknowledged:
+        return jsonify({'message': 'Coche añadido', 'car_id': str(result.inserted_id)}), 201
+    else:
+        return jsonify({'message': 'Error al procesar la solicitud'}), 400
+    
+#Encontrar los carros del usuario logeado
+@app.route('/getUserCars', methods=['GET'])
+@jwt_required()
+def getCars():
+    # Obtener el ID del usuario desde el JWT
+    user_id = get_jwt_identity()
+
+    #El user_id que esta como str hay que convertirlo a ObjectId para poder hacer la busqueda
+    user_id = ObjectId(user_id)
+
+    # Buscar en la base de datos usando el ID del usuario
+    cars = mongo.db.cars.find_one({'user_id': user_id})
+
+    if cars:
+        #Como _id es un objectid hay que volverlo str para poderlo mandar a un json
+        cars['_id'] = str(cars['_id'])
+        cars['user_id'] =str(cars['user_id'])
+        return jsonify({'message': 'Carro encontrado', 'car': cars}), 200
+    else:
+        return jsonify({'message': 'Carro no encontrado'}), 404
+
+#comentario
 
 
 # En Python, cada archivo tiene una variable especial llamada __name__.
